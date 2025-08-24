6644d681f40fe41423e62d518607d0bdeb31159c
Zhiming Chen <zhi.m.ch@gmail.com>
2025-08-25 00:29:34 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://zhimingc.github.io',
-    base: '/my-repo',
+    base: '/zmcn-site',
 });
